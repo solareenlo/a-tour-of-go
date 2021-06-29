f0ac8e334cd6431a5ab95a87f04815c1a63ad915
solareenlo <solareenlo@gmail.com>
2021-06-29 09:12:45 +0900

    add: 03_Flow-control-statements

---
+++ 03_Flow-control-statements:-for-if-else-switch-and-defer/08_exercise-loops-and-functions.go
@@ -0,0 +1,19 @@
+package main
+
+import(
+	"fmt"
+	"math"
+)
+
+func Sqrt(x float64) float64 {
+	z := 1.0
+	for d := 1.0; d*d > 1e-10; z -= d {
+		d = (z*z - x) / (2*z)
+	}
+	return z
+}
+
+func main() {
+	fmt.Println(Sqrt(2))
+	fmt.Println(math.Sqrt(2))
+}
